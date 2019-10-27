19c409c26dd743bf437453e28f4060cb09b69aa8
Édipo Nascimento <ediposnascimento@gmail.comt>
2019-10-26 21:31:08 -0300

    more Java data types

---
+++ test/Application.java
@@ -0,0 +1,29 @@
+public class Application {
+
+	public static void main(String[] args) {
+
+		int myInt = 31;
+
+		String text = "Hello!";
+
+		String blank = " ";
+
+		String name = "Édipo Nascimento";
+
+		String gretting = text + blank + name;
+
+		// output gretting on the console.
+		System.out.println(gretting);
+
+		System.out.println("Hello!" + " " + "Édipo Nascimento");
+
+		System.out.println("My integer is " + myInt);
+
+		double myDouble = 7.8; // double is a 64 bits
+
+		System.out.println(myDouble);
+
+
+	}
+
+}
