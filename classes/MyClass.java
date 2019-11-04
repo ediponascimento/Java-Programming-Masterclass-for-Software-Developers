9e1d8c14afb5d14ffbb6f3a4fc548d54bee1701b
Édipo Nascimento <ediposnascimento@gmail.comt>
2019-11-04 10:39:58 -0300

    Java construcotr part2

---
+++ classes/MyClass.java
@@ -0,0 +1,15 @@
+class MyClass {
+
+  MyClass(){} // class constructor
+
+}
+
+class DemoMyClass {
+
+  public static void main(String[] args) {
+
+    MyClass mc = new MyClass(); // when we careate a new instance of the class, the construct will be called automaticaly
+
+  }
+
+}
