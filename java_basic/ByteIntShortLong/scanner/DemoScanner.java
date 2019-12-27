eb93bfcb90c8f007550d24cdc7c90f3dcd88a40f
Édipo Nascimento <ediposnascimento@gmail.com>
2019-12-27 06:39:53 -0300

    using scanner class in Java

--- java_basic/ByteIntShortLong/scanner/DemoScanner.java
+++ java_basic/ByteIntShortLong/scanner/DemoScanner.java
@@ -8,12 +8,19 @@ public class DemoScanner {
 
         Scanner sc = new Scanner(System.in);    
         
+        // System.out.print("Entenr an int number: ");
+        // int intValue = sc.nextInt();
+
         System.out.print("Entenr an int number: ");
         int intValue = sc.nextInt();
         
+        System.out.print("Name: ");
+        String name = sc.next();
+
         sc.close();
         
         System.out.println("You typed number: " + intValue);
+        System.out.println(name);
 
         
 
